6b550c5159a83488e669380821fe26b173ef3f50
Bakus2010 <raidenmaiden2@gmail.com>
2021-12-01 02:38:57 +0100

    Api - MySQL database synchronization

--- src/Controller/CurrencyController.php
+++ src/Controller/CurrencyController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Currency;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\ORM\EntityManagerInterface;
 
 use App\Managers\ApiManager;
 
@@ -13,12 +16,13 @@ class CurrencyController extends AbstractController
     /**
      * @Route("/")
      */
-    public function index(ApiManager $apiManager): Response
+    public function index(ApiManager $apiManager, EntityManagerInterface $entityManager): Response
     {
-        $data = $apiManager->GetCurrencies('https://api.nbp.pl/api/exchangerates/tables/A');
-//        dd($data[0]->rates);
+        $data = $apiManager->handleCurrencies('https://api.nbp.pl/api/exchangerates/tables/A');
+
+        $currencies = $entityManager->getRepository(Currency::class)->findAll();
         return $this->render('currency/index.html.twig', array(
-            'data' => $data[0]->rates
+            'data' => $currencies
         ));
     }
 
--- src/Managers/ApiManager.php
+++ src/Managers/ApiManager.php
@@ -3,19 +3,65 @@
 namespace App\Managers;
 
 use App\Entity\Currency;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 
 class ApiManager
 {
 
-    public function GetCurrencies(string $json) {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
+    public function handleCurrencies(string $json) {
 
         $response = new Response();
         $response->headers->set('Content-Type', 'application/json');
 
         $api = file_get_contents($json);
 
-        return json_decode($api);
+        $data = json_decode($api);
+
+        $this->putCurrencies($data);
+
+
     }
+
+    public function putCurrencies($data) {
+        $em = $this->entityManager;
+        $count = count($data[0]->rates);
+        //dd($data[0]->rates);
+
+        for($i = 0; $i<$count; $i++) {
+            $check = $em->getRepository(Currency::class)->findBy(['currency_code' => $data[0]->rates[$i]->code]);
+            if($check) {
+
+                $qb = $em->createQueryBuilder();
+                $qb->update('App\Entity\Currency', 'Currency');
+                $qb->set('Currency.exchange_rate', ':exchange');
+                $qb->setParameter('exchange', $data[0]->rates[$i]->mid);
+                $qb->getQuery()->execute();
+
+            }else{
+
+                $create = new Currency();
+                $create->setName($data[0]->rates[$i]->currency);
+                $create->setCurrencyCode($data[0]->rates[$i]->code);
+                $create->setExchangeRate($data[0]->rates[$i]->mid);
+                $em->persist($create);
+
+            }
+
+            $em->flush();
+        }
+
+        return new Response('Updating Currency');
+    }
+
+
 
 }
